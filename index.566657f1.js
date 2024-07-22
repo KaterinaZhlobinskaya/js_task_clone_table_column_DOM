31dd7469e4ab28556564abf688438eade3e08269
Katya <evil.666.buny@gmail.com>
2024-07-22 17:30:01 +0300

    make build

---
+++ index.566657f1.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(){document.querySelector("table").querySelectorAll("tr").forEach(function(e){var t=e.querySelectorAll("th, td");if(t.length>1){var n=t[1].cloneNode(!0);e.insertBefore(n,t[t.length-1])}})});
+//# sourceMappingURL=index.566657f1.js.map
